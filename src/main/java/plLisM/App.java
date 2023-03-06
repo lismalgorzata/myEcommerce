a36bd311ea15c6a130bc70109b84a2845cd469a8
Małgorzata Lis <s222287@student.uek.krakow.pl>
2023-03-06 18:00:59 +0100

    initial for ecommerce

---
+++ src/main/java/plLisM/App.java
@@ -0,0 +1,32 @@
+package plLisM;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Jakub", "Kasia", "Michał", "Marta");
+
+        Greeter greeter = new Greeter();
+        greeter.greet("Kuba"); // -> Hello Kuba
+
+        List<String> ladies = new ArrayList<String>();
+        // Greet all ladies
+        for (String name : names) {
+            if (name.endsWith("a")) {
+                ladies.add(name);
+            }
+        }
+        for (String ladyName : ladies) {
+            greeter.greet(ladyName);
+        }
+
+        names.stream()
+                        .filter(name -> name.endsWith("a")) // Lambda name: name[-1] == "a"
+                        .filter(name -> name.startsWith("K"))
+                        .map(String::toUpperCase)
+                        .forEach(greeter::greet);
+
+    }
+}
