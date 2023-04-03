899f0a02dcb047ae46801f505ddb529d1472292c
Małgorzata Lis <s222287@student.uek.krakow.pl>
2023-04-03 15:10:24 +0200

    product catalog updates

---
+++ src/main/java/pl/mlis/productcatalog/Product.java
@@ -0,0 +1,78 @@
+package pl.mlis.productcatalog;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public class Product {
+    private final String uuid;
+    private final String name;
+    private final String desc;
+    private String image;
+    private Boolean isPublished;
+    private BigDecimal price;
+    private Boolean online;
+    private final String color;
+    private final int x;
+    private final int y;
+
+    public Product(UUID uuid, String name, String desc, String image, Boolean isPublished, BigDecimal price, String color, int x, int y) {
+        this.uuid = uuid.toString();
+        this.name = name;
+        this.desc = desc;
+        this.image = image;
+        this.isPublished = isPublished;
+        this.price = price;
+        this.color = color;
+        this.x = x;
+        this.y = y;
+    }
+
+    public String getId() {
+        return this.uuid;
+    }
+
+    public UUID getUUID() {
+        return UUID.fromString(this.uuid);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    //ERP
+    public BigDecimal getPrice() {
+        return price;
+    }
+    public void setPrice(BigDecimal price){
+        this.price = price;
+    }
+    public void changePrice(BigDecimal newPrice) {
+        price = newPrice;
+    }
+
+    //ERP
+    public String getImage() {
+        return image;
+    }
+    public void setImage(String image){
+        this.image = image;
+    }
+
+    public Boolean getIsPublished() {
+        return isPublished;
+    }
+    public void setIsPublished(Boolean isPublished){
+        if (this.image == null || this.price == null){
+            throw new ProductCannotBePublishedException();
+        }
+        this.isPublished = isPublished;
+    }
+
+    public void setOnline(boolean online) {
+
+        this.online = online;
+    }
+    public boolean getOnline() {
+        return online;
+    }
+}
---
+++ src/main/java/pl/mlis/productcatalog/ProductCannotBePublishedException.java
@@ -0,0 +1,4 @@
+package pl.mlis.productcatalog;
+
+public class ProductCannotBePublishedException extends IllegalStateException {
+}
